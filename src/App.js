03a9c667c287942cba0d75423456e66203deeff5
sayem243 <sayem243@gmail.com>
2021-07-06 21:05:05 +0600

    Render props pattern

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+import Counter from './components/Counter';
+import ClickCounter from './components/React_Render_Props/ClickCounter';
+import HoverCounter from './components/React_Render_Props/HoverCounter';
 
 function App() {
     return (
         <div className="app">
-            <ClickCounter />
-            <HoverCounter />
+            <Counter
+                render={(count, incrementCount) => (
+                    <ClickCounter count={count} incrementCount={incrementCount} />
+                )}
+            />
+            <Counter
+                render={(count, incrementCount) => (
+                    <HoverCounter count={count} incrementCount={incrementCount} />
+                )}
+            />
         </div>
     );
 }
